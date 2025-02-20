dd1736cf115236e41903b987ec378458f9d88e58
ZineddineLch <zinoufatnaahmed@gmail.com>
2025-02-20 20:05:54 +0000

    Initial commit

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,37 @@ from sklearn.linear_model import  LogisticRegression
 from sklearn.preprocessing import LabelEncoder
 def run():
    df=pd.read_csv("data/Email spam.csv")
+ 
+# Load the CSV file
+   
+
+# Convert the 'Full_Date' column to string (in case it's read as an integer or other type)
+   df['Date Survenance'] = df['Date Survenance'].astype(str)
+
+# Split the date into three columns: Day, Month, Year
+   df[['Day', 'Month', 'Year']] = df['Date Survenance'].str.split('/', expand=True)
+
+# Save the updated CSV
+   df.drop(columns=['Date Survenance'], inplace=True)
+   df["Règlement"] = df["Règlement"].astype(str).str.replace('"', '').str.strip()
+
+# Convert "Règlement" to a numeric type (float) after replacing commas with dots
+   df["Règlement"] = df["Règlement"].str.replace(',', '', regex=True).astype(float)
+   df.drop(columns=["Désignation Produit"], inplace=True)
+# Save the cleaned data
+   
+   df.to_csv("data/updated_data.csv", index=False)
+
+# Print the updated DataFrame
+
+
+
+# Load the CSV file
+
+
+# Split the column into three new columns
+   df=pd.read_csv("data/updated_data.csv")
+
    numeric_columns=df.select_dtypes(include=["number"]).columns
    df[numeric_columns]=df.select_dtypes(include=["number"]).fillna(df.select_dtypes(include=["number"]).mean())
    encoder= LabelEncoder()
